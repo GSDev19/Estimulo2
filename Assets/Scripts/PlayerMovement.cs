3cc6266c3fa3d465df02161605907fbd02e4f122
TtheNix <nahuelfacundox@gmail.com>
2024-01-27 19:44:42 -0300

    save

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 32f;
+
+ public float moveSpeed = 5f;
+    private Vector2 movementInput;
+
+    private void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        Vector2 movement = new Vector2(movementInput.x, movementInput.y);
+        Vector2 moveVelocity = movement.normalized * moveSpeed;
+        transform.Translate(moveVelocity * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 }
